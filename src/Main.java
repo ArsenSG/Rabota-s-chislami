f8f76773f7c98aba589521257d130ccad042830a
Арсен Сулейманов <ars.suleimanov@yandex.ru>
2022-05-22 17:12:42 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> sortedInt = new ArrayList<>();
+
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) > 0 && intList.get(i) % 2 == 0) {
+                sortedInt.add(intList.get(i));
+            }
+        }
+        Collections.sort(sortedInt);
+        System.out.println(sortedInt);
+    }
+}
